ad4a2203a0607cd21ebd2c15843e120910840e01
mohammadh-ghasemi <mohammadh.qasemii@gmail.com>
2025-09-20 20:22:01 +0330

    install NODE_ENV,morgan,dotenv

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const todoRouter = require("./routes/todoRoutes");
+const todoRouter = require('./routes/todoRoutes');
+const morgan = require('morgan');
 
 app.use(express.json());
-app.use("/api/v1/todos", todoRouter);
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/todos', todoRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const port = 3000;
+// console.log(app.get("env"));
+// console.log(process.env.PASSWORD);
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`app running on http://127.0.0.1:${port}`);
 });
